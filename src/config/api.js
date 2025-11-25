833d5c8df4719dc5ed46fdb13124ee8a049751fc
Gonzalo <gonzalorom2001@gmail.com.com>
2025-11-24 22:24:06 -0300

    Actualizo API Base URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 // src/config/api.js
-const API_BASE_URL = 'http://localhost/backendVistaMontana/index.php';
+const API_BASE_URL = 'https://vistamontana.free.nf/backendVistaMontana/index.php';
 
 export const apiClient = {
     get: async (endpoint) => {
